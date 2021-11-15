f75af191feec746fdb1f4054015118aafa1f2b75
Yasmine <85246232+yasmine-elkhouly@users.noreply.github.com>
2021-11-14 23:05:53 -0500

    Add files via upload

---
+++ quickAcceptLinkedIn.js
@@ -0,0 +1,7 @@
+let acceptBtn1 = document.querySelectorAll(".artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view.invitation-card__action-btn");
+
+acceptBtn1
+
+for (const btn of acceptBtn1){
+    btn.click();
+}
